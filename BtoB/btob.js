6f0dd43d709ca7a176472560224d7f46bcfd4a10
handkgroup <handkgroup@handk-inc.co.jp>
2024-12-19 22:36:18 +0500

    BtoB page finished contact pages created

--- BtoB/btob.js
+++ BtoB/btob.js
@@ -1,3 +1,59 @@
+// contact sectoin
+
+
+const formContainer = document.getElementById('form-container');
+const reviewContainer = document.getElementById('review-container');
+const thankYouContainer = document.getElementById('thank-you-container');
+const rvNoneElements = document.querySelectorAll('#rv-none');
+
+const companyInput = document.getElementById('company');
+const nameInput = document.getElementById('name');
+const furiganaInput = document.getElementById('furigana');
+const phoneInput = document.getElementById('phone');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
+const reviewCompany = document.getElementById('review-company');
+const reviewName = document.getElementById('review-name');
+const reviewFurigana = document.getElementById('review-furigana');
+const reviewPhone = document.getElementById('review-phone');
+const reviewEmail = document.getElementById('review-email');
+const reviewMessage = document.getElementById('review-message');
+
+const submitButton = document.querySelector('.submit-btn');
+const noButton = document.getElementById('no-btn');
+const yesButton = document.getElementById('yes-btn');
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    reviewCompany.textContent = companyInput.value;
+    reviewName.textContent = nameInput.value;
+    reviewFurigana.textContent = furiganaInput.value;
+    reviewPhone.textContent = phoneInput.value;
+    reviewEmail.textContent = emailInput.value;
+    reviewMessage.textContent = messageInput.value;
+
+    formContainer.style.display = 'none';
+    reviewContainer.style.display = 'flex';
+    rvNoneElements.forEach(element => element.style.display = 'none');
+});
+
+noButton.addEventListener('click', function() {
+    formContainer.style.display = 'block';
+    reviewContainer.style.display = 'none';
+    rvNoneElements.forEach(element => element.style.display = 'unset');
+});
+
+yesButton.addEventListener('click', function() {
+    reviewContainer.style.display = 'none';
+    thankYouContainer.style.display = 'flex';
+    rvNoneElements.forEach(element => element.style.display = 'none');
+});
+
+
+
+// animations
 const leftAnimal = document.querySelector('.animal-left-j');
 const rightAnimal = document.querySelector('.animal-right-j');
 
@@ -48,3 +104,8 @@ $(document).ready(function() {
   $(window).on('scroll', checkInView);
   checkInView();
 });
+
+
+
+
+
